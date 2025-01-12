093115abf4f33131d3f593757476f10f20658d01
LDS <dsliu0353@gmail.com>
2025-01-12 09:53:16 -0500

    update render h3

--- code1/render_h3.sh
+++ code1/render_h3.sh
@@ -1,5 +1,5 @@
 for ((i=1; i<=10000; i+=200)); do
     echo "正在渲染第 $i 批次的帧（从帧 $i 开始，共渲染 45 帧）"
-    ~/Downloads/blender-4.3.1-linux-x64/blender  -b -P pipeline_h3.py -- --iter $i --size 200 --resolution 128
+    /home/lds/Downloads/blender-4.3.2-linux-x64/blender  -b -P pipeline_h3_linear_fully_connected.py -- --iter $i --size 200 --resolution 128
     echo "Blender 已完成第 $i 批次的渲染，准备重启以渲染下一个批次"
 done
